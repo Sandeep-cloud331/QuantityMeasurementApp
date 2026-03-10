efb2942f595f98a24987c5dabf6a295068a50c70
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2026-03-10 10:50:23 +0530

    [Sandeep Chahar] UC9 - Implemented Weight Measurement Equality , Conversion and Addition(Kilogram, Pound, Gram)

---
+++ src/main/java/com/sandeep/quantitymeasurement/WeightUnit.java
@@ -0,0 +1,24 @@
+package com.sandeep.quantitymeasurement;
+
+public enum WeightUnit {
+    KILOGRAM(1.0),
+    GRAM(0.001),
+    POUND(0.453592);
+
+    private final double toKGFactor;
+
+    WeightUnit(double toKGFactor){
+        this.toKGFactor = toKGFactor;
+    }
+
+    public double convertToKg(double value){
+        return value * toKGFactor;
+    }
+
+    public double convertFromKg(double value){
+        return value/toKGFactor;
+    }
+
+    public double getToKgFactor() { return toKGFactor; }
+}
+
